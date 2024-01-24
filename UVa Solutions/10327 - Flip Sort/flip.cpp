c29d0d3c96e2af7dbf19650ec25a1e2a47111bf5
Mateusz Sobkowiak <60814050+Sobgo@users.noreply.github.com>
2024-01-24 22:46:25 +0100

    UVa/10327
    This one is easy but i did it in a weird way so I thought it's worth uploading

---
+++ UVa Solutions/10327 - Flip Sort/flip.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+typedef long long int lli;
+
+lli counter = 0;
+lli total = 0;
+
+template <typename RandomIt>
+void mergesort(RandomIt first, RandomIt last) {
+    if (last - first > 1) {
+        RandomIt middle = first + (last - first) / 2;
+       
+        mergesort(first, middle);
+        mergesort(middle, last);
+
+        counter = middle - first;
+
+		using value_type = typename iterator_traits<RandomIt>::value_type;
+
+        inplace_merge(first, middle, last, [](const value_type &a, const value_type &b) mutable {
+            if (a < b) {
+                total += counter;
+                return true;
+            }
+            counter--;
+            return false;
+        });
+    }
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+
+	while (cin >> n) {
+		vector <lli> v;
+		lli x;
+
+		for (int i = 0; i < n; ++i) {
+			cin >> x;
+			v.push_back(x);
+		}
+
+		mergesort(v.begin(), v.end());
+		cout << "Minimum exchange operations : " << total << "\n";
+		total = 0;
+	}
+
+	return 0;
+}
